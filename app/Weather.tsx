e9811b681881a0576b6a8eae351b2b6a72fbfde6
Brenley Dueck <brenleydueck@gmail.com>
2023-06-23 21:15:20 -0500

    Use server actions

--- app/Weather.tsx
+++ app/Weather.tsx
@@ -1,55 +1,30 @@
 'use client';
 
-import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
+import { useState } from "react";
 
 type Temperature = {
     kelvin: number;
     celcius: number;
 }
 
-export default function Weather({ temperature, cityName }: { temperature?: Temperature, cityName: string }) {
-    const router = useRouter();
-
+export default function Temperature({ temperature, cityName }: { temperature: Temperature, cityName: string }) {
     const [tempUnit, setTempUnit] = useState('Celcius');
-
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-
-        const form = e.currentTarget as HTMLFormElement;
-        const formElements = form.elements as typeof form.elements & {
-            cityName: HTMLInputElement;
-        };
-        const cityName = formElements.cityName.value;
-
-        router.push('/?city=' + cityName)
-
-        form.reset();
-        formElements.cityName.focus();
-    };
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <input name="cityName" placeholder="City name" />
-                <button type="submit">
-                    Check Weather
-                </button>
-            </form>
-            {temperature && (<><p>
+            <p>
                 The current weather in <strong>{cityName}</strong> is:{' '}
                 {tempUnit === 'Celcius' ? temperature.celcius : temperature.kelvin}
             </p>
-                <p>
-                    <button onClick={() => {
-                        setTempUnit('Celcius');
-                    }}>Celcius</button>
+            <p>
+                <button onClick={() => {
+                    setTempUnit('Celcius');
+                }}>Celcius</button>
 
-                    <button onClick={() => {
-                        setTempUnit('Kelvin');
-                    }}>Kelvin</button>
-                </p>
-            </>)}
+                <button onClick={() => {
+                    setTempUnit('Kelvin');
+                }}>Kelvin</button>
+            </p>
         </>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
+import { revalidatePath } from "next/cache";
 import { getTemperature } from "./actions";
-import Weather from "./Weather";
+import Temperature from "./Weather";
 
 export default async function Home({ searchParams }: any) {
   const cityName = searchParams.city || '';
   const temperature = await getTemperature(cityName);
 
+  const handleSubmit = async (formData: FormData) => {
+    'use server';
+    const cityName = formData.get('cityName');
+    // how do i reload the server component with the proper city?
+  };
+
   return (
     <>
       <h2>Weather</h2>
-      <Weather cityName={cityName} temperature={temperature} />
+      <form action={handleSubmit}>
+        <input name="cityName" placeholder="City name" />
+        <button type="submit">
+          Check Weather
+        </button>
+      </form>
+      {temperature && <Temperature temperature={temperature} cityName={cityName} />}
     </>
   );
 }
